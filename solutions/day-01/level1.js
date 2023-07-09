86174f16ae818372470d59c552b55eb0f5f255f5
YuricJulias <yuricjulias@gmail.com>
2023-07-09 21:44:43 +0700

    chore: exercise level1 complete

---
+++ solutions/day-01/level1.js
@@ -0,0 +1,105 @@
+// 1
+// const arr = [];
+
+// 2
+// const number = [1, 12.4, 89, 5, 10, 2];
+
+// 3
+// console.log(number.length)
+
+// 4
+// console.log(number[0]);
+// console.log(number[Math.round((number.length - 1) / 2)]);
+// console.log(number[number.length - 1]);
+
+// 5
+// const mixedDataTypes = [3.14, "John", "C", 1, [45, 56, 67], true];
+
+// console.log(mixedDataTypes.length)
+
+// 6
+const itCompanies = []
+
+itCompanies.push("Facebook")
+itCompanies.push("Google")
+itCompanies.push("Microsoft")
+itCompanies.push("Apple")
+itCompanies.push("IBM")
+itCompanies.push("Oracle")
+itCompanies.push("Amazon")
+
+// 7
+// console.log(itCompanies)
+
+// 8
+// console.log(itCompanies.length)
+
+// 9
+// console.log(itCompanies[0])
+// console.log(itCompanies[Math.floor((itCompanies.length-1)/2)])
+// console.log(itCompanies[itCompanies.length-1])
+
+// 10
+// console.log(itCompanies[0])
+// console.log(itCompanies[1])
+// console.log(itCompanies[2])
+// console.log(itCompanies[3])
+// console.log(itCompanies[4])
+// console.log(itCompanies[5])
+// console.log(itCompanies[6])
+
+// 11
+// console.log(itCompanies[0].toUpperCase())
+// console.log(itCompanies[1].toUpperCase())
+// console.log(itCompanies[2].toUpperCase())
+// console.log(itCompanies[3].toUpperCase())
+// console.log(itCompanies[4].toUpperCase())
+// console.log(itCompanies[5].toUpperCase())
+// console.log(itCompanies[6].toUpperCase())
+
+// 12
+// console.log(itCompanies.join(', ') + " are big IT companies.")
+
+// 13
+// console.log(itCompanies.includes("Amazon"))
+// console.log(itCompanies.includes("Tokopedia"))
+
+// 14
+
+
+// 15
+// console.log(itCompanies.sort())
+
+// 16
+// console.log((itCompanies.sort()).reverse())
+
+// 17
+// console.log(itCompanies.slice(0, 3))
+
+// 18
+// console.log(itCompanies.slice(itCompanies.length-3, itCompanies.length))
+
+// 19
+console.log(itCompanies)
+// console.log(itCompanies.slice(Math.floor((itCompanies.length-1)/2) , Math.floor((itCompanies.length-1)/2) + 1))
+
+
+// 20
+itCompanies.shift(0)
+console.log(itCompanies)
+
+// 21
+itCompanies.shift(Math.floor((itCompanies.length-1)/2))
+console.log(itCompanies)
+
+// 22
+itCompanies.shift(itCompanies.length-1)
+console.log(itCompanies)
+
+// 23
+itCompanies.splice(0)
+console.log("Ini Splice semua " + itCompanies)
+
+
+
+
